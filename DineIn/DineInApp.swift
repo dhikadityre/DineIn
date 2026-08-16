fad1df430ad3557211bf060caacbff46a8bb52e6
@algoreadme <dhikaaditya1@gmail.com>
2026-08-16 12:52:08 +0700

    Initial Commit

---
+++ DineIn/DineInApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineInApp.swift
+//  DineIn
+//
+//  Created by DHIKA ADITYA ARE on 16/08/26.
+//
+
+import SwiftUI
+
+@main
+struct DineInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
