85346c8493f0c9949cf1cdb9a93425ec76893e4e
@algoreadme <dhikaaditya1@gmail.com>
2026-08-16 13:38:20 +0700

    feat: refactor ItemMenu view to include styled thumbnails and dietary restriction indicators

--- DineIn/Presentation/Menu/Components/ItemMenu.swift
+++ DineIn/Presentation/Menu/Components/ItemMenu.swift
@@ -9,22 +9,52 @@ import SwiftUI
 
 struct ItemMenu: View {
     let item: MenuItem
+    let colors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green
+    ]
     
     var body: some View { render() }
     
     private func render() -> some View {
         HStack {
-            Image(item.thumbnailImage)
-            VStack(alignment: .leading) {
-                Text(item.name)
-                Text("$\(item.price)")
-            }
+            renderThumbnail()
+            renderContent()
+            Spacer()
+            renderRestriction()
         }
     }
 }
 
 extension ItemMenu {
+    private func renderThumbnail() -> some View {
+        Image(item.thumbnailImage)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(.gray, lineWidth: 2))
+    }
     
+    private func renderContent() -> some View {
+        VStack(alignment: .leading) {
+            Text(item.name)
+                .font(.headline)
+            Text("$\(item.price)")
+        }
+    }
+    
+    private func renderRestriction() -> some View {
+        ForEach(item.restrictions, id: \.self) { restriction in
+            Text(restriction)
+                .font(.caption)
+                .fontWeight(.black)
+                .padding(5)
+                .background(colors[restriction, default: .black])
+                .clipShape(Circle())
+                .foregroundStyle(.white)
+        }
+    }
 }
 
 #Preview {
