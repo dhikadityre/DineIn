0e9a8f8b4615b2a479a8db60fb1ee5b365373617
@algoreadme <dhikaaditya1@gmail.com>
2026-08-16 13:16:22 +0700

    feat: implement MenuScreen with JSON decoding support and replace ContentView

---
+++ DineIn/Presentation/MenuScreen.swift
@@ -0,0 +1,33 @@
+//
+//  MenuScreen.swift
+//  DineIn
+//
+//  Created by DHIKA ADITYA ARE on 16/08/26.
+//
+
+import SwiftUI
+
+struct MenuScreen: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View { render() }
+    
+    private func render() -> some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+        }
+    }
+}
+
+#Preview {
+    MenuScreen()
+}
